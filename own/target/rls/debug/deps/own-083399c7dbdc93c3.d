a46730ba880d4eba24abb204b4d34e12d2229674
UGOI <stefan@cryptoledgermatrix.com>
2022-02-09 10:47:52 +0000

    divisors function now working

--- own/target/rls/debug/deps/own-083399c7dbdc93c3.d
+++
@@ -1,5 +0,0 @@
-/workspace/ProgrammingRust/own/target/rls/debug/deps/own-083399c7dbdc93c3.rmeta: src/main.rs
-
-/workspace/ProgrammingRust/own/target/rls/debug/deps/own-083399c7dbdc93c3.d: src/main.rs
-
-src/main.rs:
